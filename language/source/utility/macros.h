e0cbe45cfa795ab338dc41ae8d7a1d5a655b1221
Šimon Tupý <simontupy64@gmail.com>
2023-04-02 16:15:44 +0200

    Cleaned up the macro file.

--- language/source/utility/macros.h
+++ language/source/utility/macros.h
@@ -2,43 +2,29 @@
 
 #include "types.h"
 
-namespace channel {
-	using namespace types;
-}
-
-#define LANG_REPORT_BASE(...) (void) ((1 != _CrtDbgReport(__VA_ARGS__)) || (__debugbreak(), 0))
-#define LANG_REPORT_F0(rptno, msg) LANG_REPORT_BASE(rptno, __FILE__, __LINE__, NULL, "%s", msg)
-
-#define LANG__CRT_SECURE_INVALID_PARAMETER(expr) ::_invalid_parameter(_CRT_WIDE(#expr), L"", __FILEW__, __LINE__, 0)
-
-#define LANG_REPORT_ERROR(mesg)                   \
-do {                                              \
-        LANG_REPORT_F0(_CRT_ASSERT, mesg);        \
-        LANG__CRT_SECURE_INVALID_PARAMETER(mesg); \
-} while (false)
+//#define LANG_REPORT_BASE(...) (void) ((1 != _CrtDbgReport(__VA_ARGS__)) || (__debugbreak(), 0))
+//#define LANG_REPORT_F0(rptno, msg) LANG_REPORT_BASE(rptno, __FILE__, __LINE__, NULL, "%s", msg)
+//
+//#define LANG__CRT_SECURE_INVALID_PARAMETER(expr) ::_invalid_parameter(_CRT_WIDE(#expr), L"", __FILEW__, __LINE__, 0)
+//
+//#define LANG_REPORT_ERROR(mesg)                   \
+//do {                                              \
+//        LANG_REPORT_F0(_CRT_ASSERT, mesg);        \
+//        LANG__CRT_SECURE_INVALID_PARAMETER(mesg); \
+//} while (false)
 
 #define LANG_VERIFY(cond, mesg)                                     \
     do {                                                            \
         if (cond) { /* contextually convertible to bool paranoia */ \
         } else {                                                    \
-            LANG_REPORT_ERROR(mesg);                                \
+            std::cout << mesg << '\n';                              \
+            __debugbreak();                                         \
         }                                                           \
-                                                                    \
-        _Analysis_assume_(cond);                                    \
     } while (false)
 
-#ifdef _DEBUG
 /**
  * \brief Basic assertion macro, asserts whenever \a cond evaluates to false and prints \a message.
  * \param cond Condition to evaluate
  * \param mesg Assertion notification message
  */
 #define ASSERT(cond, mesg) LANG_VERIFY(cond, mesg)
-#else
-/**
- * \brief Basic assertion macro, asserts whenever \a cond evaluates to false and prints \a message.
- * \param cond Condition to evaluate
- * \param mesg Assertion notification message
- */
-#define ASSERT(cond, mesg)
-#endif
--- language/source/utility/types.h
+++ language/source/utility/types.h
@@ -7,20 +7,24 @@
 #include <map>
 #include <unordered_map>
 
-namespace channel::types {
-	// signed
-	using i8  = int8_t;
-	using i16 = int16_t;
-	using i32 = int32_t;
-	using i64 = int64_t;
+namespace channel {
+	namespace types {
+		// signed
+		using i8 = int8_t;
+		using i16 = int16_t;
+		using i32 = int32_t;
+		using i64 = int64_t;
 
-	// unsigned 
-	using u8  = uint8_t;
-	using u16 = uint16_t;
-	using u32 = uint32_t;
-	using u64 = uint64_t;
+		// unsigned 
+		using u8 = uint8_t;
+		using u16 = uint16_t;
+		using u32 = uint32_t;
+		using u64 = uint64_t;
 
-	// floating point
-	using f32  = float;
-	using f64  = double;
+		// floating point
+		using f32 = float;
+		using f64 = double;
+	}
+
+	using namespace types;
 }

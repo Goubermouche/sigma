84e9cbca8b3be4c78b62c8b55d288d245cef8ade
Šimon Tupý <simontupy64@gmail.com>
2023-10-26 23:15:06 +0200

    Implemented a TB-inspired IR function and symbol scheme.

---
+++ source/intermediate_representation/nodes/function.cpp
@@ -0,0 +1,121 @@
+#include "function.h"
+#include <utility/filesystem/file_types/text_file.h>
+
+namespace ir {
+	function::function(const symbol& symbol) :
+		m_allocator(NODE_ALLOCATION_BLOCK_SIZE),
+		m_symbol(symbol) {
+		m_parameters.resize(3);
+	}
+
+	void function::print_node_graph(s_ptr<utility::text_file> file) const {
+		file << "digraph " << m_symbol.get_name() << "{\n";
+		file << "  rankdir=TB\n";
+
+		std::unordered_set<handle<node>> visited;
+		m_entry_node->print_as_basic_block(visited, file);
+
+		file << "}\n";
+	}
+
+	void function::set_entry_node(handle<node> node) {
+		m_entry_node = node;
+	}
+
+	handle<node> function::get_entry_node() const {
+		return m_entry_node;
+	}
+
+	void function::set_exit_node(handle<node> node) {
+		m_exit_node = node;
+	}
+
+	handle<node> function::get_exit_node() const {
+		return m_exit_node;
+	}
+
+	void function::set_active_control_node(handle<node> node) {
+		m_active_control_node = node;
+	}
+
+	handle<node> function::get_active_control_node() const {
+		return m_active_control_node;
+	}
+
+	u64 function::get_control_node_count() const {
+		return m_control_node_count;
+	}
+
+	void function::set_control_node_count(u64 count) {
+		m_control_node_count = count;
+	}
+
+	u64 function::get_node_count() const {
+		return m_node_count;
+	}
+
+	void function::set_node_count(u64 node_count) {
+		m_node_count = node_count;
+	}
+
+	symbol& function::get_symbol() {
+		return m_symbol;
+	}
+
+	const symbol& function::get_symbol() const {
+		return m_symbol;
+	}
+
+	void function::set_symbol(const symbol& symbol) {
+		m_symbol = symbol;
+	}
+
+	i32 function::get_parameter_count() const {
+		return m_parameter_count;
+	}
+
+	void function::set_return_count(i32 return_count) {
+		m_return_count = return_count;
+	}
+
+	void function::set_parameter(u64 index, handle<node> value) {
+		m_parameters[index] = value;
+	}
+
+	i32 function::get_return_count() const {
+		return m_return_count;
+	}
+
+	void function::add_input_late(
+		handle<node> n,
+		handle<node> input
+	) {
+		ASSERT(
+			n->get_type() == node::type::region || 
+			n->get_type() == node::type::phi,
+			"invalid node, cannot append input to a region/phi node"
+		);
+
+		const u64 old_count = n->m_inputs.get_size();
+		const u64 new_count = n->m_inputs.get_size() + 1;
+
+		// reallocate the necessary space again
+		utility::slice<handle<node>> new_inputs(
+			m_allocator.allocate(sizeof(handle<node>) * new_count), new_count
+		);
+
+		memcpy(new_inputs.get_data(), n->get_inputs().get_data(), old_count * sizeof(handle<node>));
+		new_inputs[old_count] = input;
+
+		n->m_inputs = new_inputs;
+	}
+
+	std::vector<handle<node>>& function::get_parameters() {
+		return m_parameters;
+	}
+
+	handle<node> function::get_parameter(u64 index) const {
+		return m_parameters[index];
+	}
+
+}
---
+++ source/intermediate_representation/nodes/function.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <utility/containers/allocators/block_allocator.h>
+
+#include "intermediate_representation/nodes/user.h"
+#include "intermediate_representation/nodes/symbol.h"
+
+// NOTE: it might be a good idea to determine block size from the node count
+//       of the preceding AST and scale relative to that
+#define NODE_ALLOCATION_BLOCK_SIZE 1024
+
+namespace ir {
+	class function {
+	public:
+		function(const symbol& symbol);
+
+		void set_entry_node(handle<node> node);
+		void set_exit_node(handle<node> node);
+		void set_active_control_node(handle<node> node);
+		void set_control_node_count(u64 count);
+		void set_node_count(u64 node_count);
+		void set_symbol(const symbol& symbol);
+		void set_return_count(i32 return_count);
+		void set_parameter(u64 index, handle<node> value);
+
+		handle<node> get_entry_node() const;
+		handle<node> get_exit_node() const;
+		handle<node> get_active_control_node() const;
+		u64 get_control_node_count() const;
+		u64 get_node_count() const;
+		symbol& get_symbol();
+		const symbol& get_symbol() const;
+		i32 get_parameter_count() const;
+		i32 get_return_count() const;
+		std::vector<handle<node>>& get_parameters();
+		handle<node> get_parameter(u64 index) const;
+
+		void print_node_graph(s_ptr<utility::text_file> file) const;
+
+		/**
+		 * \brief Allocates a new node, including enough space for the \a extra
+		 * \a type and enough input node pointers.
+		 * \tparam extra_type Extra type that is contained in the given node
+		 * \param type Node type of the node
+		 * \param input_count Count of inputs to allocate space for
+		 * \return Newly allocated node.
+		 */
+		template<typename extra_type>
+		handle<node> allocate_node(node::type type, u64 input_count);
+
+		/**
+		 * \brief Appends a new input to the list of inputs of the \a n node.
+		 * Due to the input nodes being stored next to their respective node
+		 * we have to reallocate the entire buffer of inputs.
+		 * \param n Node to add a new input to
+		 * \param input Input node
+		 */
+		void add_input_late(handle<node> n, handle<node> input);
+	private:
+		handle<node> m_active_control_node;
+		handle<node> m_entry_node;
+		handle<node> m_exit_node;
+
+		u64 m_control_node_count = 0;
+		i32 m_parameter_count = 0;
+		i32 m_return_count = 0;
+		u64 m_node_count = 0;
+
+		// list of nodes which are considered parameters
+		// 0 - m_parameter_count             : actual parameters
+		// m_parameter_count - m_return_count: function returns
+		std::vector<handle<node>> m_parameters;
+
+		// allocator which is used for allocating child nodes
+		utility::block_allocator m_allocator;
+		symbol m_symbol; // symbol of the given function
+	};
+
+	template<typename extra_type>
+	handle<node> function::allocate_node(node::type type, u64 input_count) {
+		void* node_allocation = m_allocator.allocate(sizeof(node));
+		const handle node_ptr = static_cast<node*>(node_allocation);
+
+		// allocate enough space for input nodes
+		node_ptr->m_inputs = utility::slice<handle<node>>(m_allocator.allocate(sizeof(handle<node>) * input_count), input_count);
+		memset(node_ptr->m_inputs.get_data(), 0, input_count * sizeof(handle<node>));
+
+		// assign data
+		node_ptr->set_type(type);
+		node_ptr->set_property(m_allocator.allocate(sizeof(extra_type)));
+		node_ptr->set_global_value_index(m_node_count++);
+
+		return node_ptr;
+	}
+}
